0d0536245e916e7f5166bbbcf6c486b8915d5da6
Linkd-Aether <jackmcevoy15@gmail.com>
2019-11-13 19:42:01 -0500

    Changed Types of Drive Motors to Victors

--- src/main/java/frc/robot/subsystems/Manipulator.java
+++ src/main/java/frc/robot/subsystems/Manipulator.java
@@ -11,7 +11,7 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 
 // import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.*;
@@ -27,8 +27,8 @@ public class Manipulator extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
   // public Solenoid leftWrist = new Solenoid(1, 6);
-  public WPI_TalonSRX leftCollector = new WPI_TalonSRX(7);
-  public WPI_TalonSRX rightCollector = new WPI_TalonSRX(6);
+  public WPI_VictorSPX leftCollector = new WPI_VictorSPX(22);
+  public WPI_VictorSPX rightCollector = new WPI_VictorSPX(23);
 
   public Manipulator() {
     leftCollector.configFactoryDefault();
