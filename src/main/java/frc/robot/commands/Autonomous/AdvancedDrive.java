c346b0b9a85c6ab7472633e3ee91a2d1cab764d3
Celia <36650015+classygirl44@users.noreply.github.com>
2019-11-18 20:58:25 -0500

    Update AdvancedDrive.java

--- src/main/java/frc/robot/commands/Autonomous/AdvancedDrive.java
+++ src/main/java/frc/robot/commands/Autonomous/AdvancedDrive.java
@@ -1,24 +1,30 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.subsystems.*;
+import java.lang.Math;
 
 public class AdvancedDrive extends Command {
     private double speed;
     private double dist;
     private double timeout;
     private double originalDistance;
+    private Drivetrain m_drive;
+    private double currentDistance;
+    private double dist;
 
-    public AdvancedDrive(double speed, double dist, double timeout) {
+    public AdvancedDrive(double speed, double dist, double timeout, Drivetrain drive) {
         this.speed = speed;
         this.dist = Math.abs(dist);
         this.timeout = timeout;
-        originalDistance = encoder.getPosition();
+        m_drive = drive;
+        originalDistance = drive.getPosition();
     }
 
     protected void execute() {
-        currentDistance = encoder.getPosition();
-        double distanceTraveled = abs(currentDistance - originalDistance);
-        if distanceTraveled >= dist {
+        currentDistance = m_drive.getPosition();
+        double distanceTraveled = Math.abs(currentDistance - originalDistance);
+        if (distanceTraveled >= dist) {
             // Stop motors and return
         } else {
             // Continue the motors
